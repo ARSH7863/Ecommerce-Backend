c98aa9ae8e57a25445a77808d1fb15fcf70af76d
Arsh7863 <arshs287@gmail.com.com>
2021-08-26 12:27:40 +0530

    Image Size Changed

--- controllers/product.js
+++ controllers/product.js
@@ -55,7 +55,7 @@ exports.create = (req, res) => {
 
     if (files.photo) {
       // console.log("FILES PHOTO: ", files.photo);
-      if (files.photo.size > 1000000) {
+      if (files.photo.size > 2(1024 * 1024)) {
         return res.status(400).json({
           error: "Image should be less than 1mb in size",
         });
